a82e4c71aa9e61657660efb67fa13b1f705a2cd3
Tan Ha <tan.ha@wave5.nl>
2023-04-17 21:35:03 +0700

    Add Stuff

--- InternshipApp.Portal/Views/Common/CommandBarView.razor.cs
+++ InternshipApp.Portal/Views/Common/CommandBarView.razor.cs
@@ -1,5 +1,15 @@
-﻿namespace InternshipApp.Portal.Views;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace InternshipApp.Portal.Views;
 
 public partial class CommandBarView
 {
+    [Parameter]
+    public EventCallback ChatCallback { get; set; }
+
+    [Parameter]
+    public EventCallback SendEmailCallback { get; set; }
+
+    [Parameter]
+    public EventCallback ChatCallback { get; set; }
 }
