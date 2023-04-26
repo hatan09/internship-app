3bb6a6f9fd37ba3b8fa94806bdd7fe11be1b9c4a
Tan Ha <tan.ha@wave5.nl>
2023-04-26 14:19:32 +0700

    Update skill tags, intern, intern evaluation

--- InternshipApp.Portal/Views/Common/SkillTag.razor.cs
+++ InternshipApp.Portal/Views/Common/SkillTag.razor.cs
@@ -1,5 +1,11 @@
-﻿namespace InternshipApp.Portal.Views;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace InternshipApp.Portal.Views;
 public partial class SkillTag
 {
+    [Parameter]
+    public string Name { get; set; } = "";
 
+    [Parameter]
+    public string Description { get; set; } = "";
 }
